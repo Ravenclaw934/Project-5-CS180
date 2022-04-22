30a0f402a870135ed3052f99fe6a6bb480d5af82
buttervader27 <102623064+buttervader27@users.noreply.github.com>
2022-04-22 14:15:11 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -5,11 +5,11 @@ public class Client {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 2222);
+            Thread t = new Thread(new LoginGUI());
+            t.start();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        Thread t = new Thread(new LoginGUI());
-        t.start();
     }
 }
