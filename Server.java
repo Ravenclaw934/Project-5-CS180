5f2a1d39c26122415053360443c45362f33e56d1
buttervader27 <102623064+buttervader27@users.noreply.github.com>
2022-04-22 13:47:43 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,16 +2,6 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
-/**
- * A class that runs a server for the button game
- *
- * <p>Purdue University -- CS18000 -- Spring 2022 -- Homework 11 -- Challenge</p>
- *
- * port number used is 2222
- *
- * @author Logan Knight
- * @version April 7, 2022
- */
 public class Server {
 
     public static void main(String[] args) {
