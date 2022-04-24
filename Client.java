e48b0f2efbf40a518e56edb88f976e9a61a205a4
buttervader27 <102623064+buttervader27@users.noreply.github.com>
2022-04-24 18:32:02 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,13 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Client {
 
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 2222);
-            Thread t = new Thread(new LoginGUI());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+            writer.println("Login");
+            writer.flush();
+            ArrayList<Student> students = (ArrayList<Student>) ois.readObject();
+            ArrayList<Teacher> teachers = (ArrayList<Teacher>) ois.readObject();
+            ArrayList<Course> courses = (ArrayList<Course>) ois.readObject();
+
+            writer.close();
+            ois.close();
+            Thread t = new Thread(new LoginGUI(students, teachers));
             t.start();
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
