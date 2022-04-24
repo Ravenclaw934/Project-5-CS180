54de09319784812454eedfe39b47e1c066406b7c
buttervader27 <102623064+buttervader27@users.noreply.github.com>
2022-04-24 18:32:21 -0400

    Update ServerThread.java

--- ServerThread.java
+++ ServerThread.java
@@ -1,8 +1,7 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ServerThread implements Runnable {
 
@@ -13,6 +12,33 @@ public class ServerThread implements Runnable {
     }
 
     public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 
+            while (true) {
+                String request = reader.readLine();
+
+                if (request.equals("Login")) {
+                    StudentImport stuImp = new StudentImport("StudentFile.txt", new ArrayList<>());
+                    TeacherImport teachImp = new TeacherImport("TeacherFile.txt", new ArrayList<>());
+                    DiscussionImport discImp = new DiscussionImport("CourseFile.txt");
+
+                    ArrayList<Student> students = stuImp.initializeStudents();
+                    ArrayList<Teacher> teachers = teachImp.initializeTeachers();
+                    ArrayList<Course> courses = discImp.initializeDiscussions(teachers, students);
+
+                    oos.writeObject(students);
+                    oos.flush();
+                    oos.writeObject(teachers);
+                    oos.flush();
+                    oos.writeObject(courses);
+                    oos.flush();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
