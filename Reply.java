ae8b45cb771c97614886f77d0728769ece85b77e
buttervader27 <102623064+buttervader27@users.noreply.github.com>
2022-04-26 15:55:32 -0400

    Update Reply.java

--- Reply.java
+++ Reply.java
@@ -1,29 +1,30 @@
+import java.io.Serializable;
 
-public class Reply implements Post
+public class Reply implements Post, Serializable
 {
-	
+
 	User poster;
 	String message;
 	int votes = 0;
-	
+
 	public Reply(User poster, String message) {
 		this.poster = poster;
 		this.message = message;
 		this.votes = 0;
 	}
-	
+
 	public Reply(User poster, String message, int votes)
 	{
 		this.poster = poster;
 		this.message = message;
 		this.votes = votes;
 	}
-	
+
 	public void editMessage(String message)
 	{
 		this.message = message;
 	}
-	
+
 	public void addVote()
 	{
 		votes++;
@@ -38,7 +39,7 @@ public class Reply implements Post
 	public User getPoster() throws ActionFailedException {
 		return poster;
 	}
-	
+
 	public int getVotes() throws ActionFailedException
 	{
 		return votes;
