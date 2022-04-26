4e759cab16bcfe5fd1e1fba279c413ca3a931101
buttervader27 <102623064+buttervader27@users.noreply.github.com>
2022-04-26 15:56:50 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,29 +1,30 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Student implements User 
+public class Student implements User, Serializable
 {
-	
+
 	String username;
 	String password;
 	boolean voted = false;
 	int grade = 0;
 	ArrayList<Course> courses;
 
-	public Student(String username, String password) 
+	public Student(String username, String password)
 	{
 		this.username = username;
 		this.password = password;
 		courses = new ArrayList<Course>();
 	}
-	
-	public Student(String username, String password, String grade) 
+
+	public Student(String username, String password, String grade)
 	{
 		this.username = username;
 		this.password = password;
 		courses = new ArrayList<Course>();
 		this.grade = Integer.parseInt(grade);
 	}
-	
+
 	public Student(String username, String password, boolean voted, int grade, ArrayList<Course> courses)
 	{
 		this.username = username;
@@ -32,74 +33,74 @@ public class Student implements User
 		this.grade = grade;
 		this.courses = courses;
 	}
-	
+
 
 	@Override
-	public Discussion postDiscussion(String message) throws PermissionDeniedException 
+	public Discussion postDiscussion(String message) throws PermissionDeniedException
 	{
-		
+
 		throw new PermissionDeniedException("You do not have permission to post discussions");
-		
+
 	}
 
 	@Override
-	public Reply reply(String message) throws PermissionDeniedException 
+	public Reply reply(String message) throws PermissionDeniedException
 	{
-		
+
 		Reply r = new Reply(this, message);
-		
+
 		return r;
-		
+
 	}
 
 	@Override
-	public String getUsername() 
+	public String getUsername()
 	{
 		return username;
 	}
 
 	@Override
-	public String getPassword() 
+	public String getPassword()
 	{
 		return password;
 	}
 
 	@Override
 	public void setUsername(String username) throws AccountExistsException {
-		
+
 		this.username = username;
-		
+
 	}
 
 	@Override
 	public void setPassword(String password)
 	{
-		
+
 		this.password = password;
 	}
 
 	@Override
-	public Discussion editDiscussion(String message) throws PermissionDeniedException, ActionFailedException 
+	public Discussion editDiscussion(String message) throws PermissionDeniedException, ActionFailedException
 	{
-		
+
 		throw new PermissionDeniedException("You do not have permission to edit discussions");
-		
+
 	}
 
 	@Override
 	public Reply postReply(String message, Discussion post) throws ActionFailedException {
-		
+
 		Reply r = new Reply(this, message);
 		return r;
-		
+
 	}
-	
+
 	public int getGrade() {
-		
+
 		return grade;
-	
+
 	}
-	
+
 	public void setGrade(int grade) {
 		this.grade = grade;
 	}
