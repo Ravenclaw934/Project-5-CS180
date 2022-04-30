3f5df5df41d7f890d84164c4ab63d0abe294121a
Ravenclaw934 <102623088+Ravenclaw934@users.noreply.github.com>
2022-04-30 15:28:56 -0400

    Update PermissionDeniedException.java

--- PermissionDeniedException.java
+++ PermissionDeniedException.java
@@ -1,4 +1,13 @@
-
+/**
+ * PermissionDeniedException
+ *
+ * An exception to be thrown when permission is denied
+ *
+ * @author Anthony Whittle, L02
+ *
+ * @version 04/30/2022
+ *
+ */
 public class PermissionDeniedException extends Exception
 {
 
