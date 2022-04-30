e78c3e95852512073dc2e50557c34dd353e14b44
Ravenclaw934 <102623088+Ravenclaw934@users.noreply.github.com>
2022-04-30 15:25:01 -0400

    Update AccountExistsException.java

--- AccountExistsException.java
+++ AccountExistsException.java
@@ -1,3 +1,13 @@
+/**
+ * AccountExistsException
+ *
+ * An exception for if the account exists
+ *
+ * @author Anthony Whittle, L02
+ *
+ * @version 04/30/2022
+ *
+ */
 
 public class AccountExistsException extends Exception {
 
