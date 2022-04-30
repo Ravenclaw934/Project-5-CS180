ccca27da8b907d70082aef53c0ccde7e7555839c
Ravenclaw934 <102623088+Ravenclaw934@users.noreply.github.com>
2022-04-30 15:25:30 -0400

    Update ActionFailedException.java

--- ActionFailedException.java
+++ ActionFailedException.java
@@ -1,4 +1,13 @@
-
+/**
+ * ActionFailedException
+ *
+ * Exception for if the action fails
+ *
+ * @author Anthony Whittle, L02 
+ *
+ * @version 04/30/2022
+ *
+ */
 public class ActionFailedException extends Exception
 {
 
