40d974819fff888af929f28a284cf0a25c4a63be
Ravenclaw934 <102623088+Ravenclaw934@users.noreply.github.com>
2022-04-30 15:28:36 -0400

    Update NoDiscussionPermissionException.java

--- NoDiscussionPermissionException.java
+++ NoDiscussionPermissionException.java
@@ -1,4 +1,13 @@
-
+/**
+ * NoDiscussionPermissionException
+ *
+ * An exception to be thrown when you don't have permission
+ *
+ * @author Anthony Whittle, L02
+ *
+ * @version 04/30/2022
+ *
+ */
 public class NoDiscussionPermissionException extends Exception
 {
 
