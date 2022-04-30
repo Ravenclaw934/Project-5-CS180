d363f821f9d1239c37735f0a85f995557754fd79
Ravenclaw934 <102623088+Ravenclaw934@users.noreply.github.com>
2022-04-30 15:29:55 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,19 +2,30 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
+/**
+ * Server
+ *
+ * This is the server side of the program
+ *
+ * @author Logan Knight, L02
+ *
+ * @version 04/30/2022
+ *
+ */
+
 public class Server {
 
     public static void main(String[] args) {
 
         try {
-        ServerSocket serverSocket = new ServerSocket(2222);
+            ServerSocket serverSocket = new ServerSocket(2222);
 
-        while (true) {
-            Socket socket = serverSocket.accept();
+            while (true) {
+                Socket socket = serverSocket.accept();
 
-            Thread t = new Thread(new ServerThread(socket));
-            t.start();
-        }
+                Thread t = new Thread(new ServerThread(socket));
+                t.start();
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
