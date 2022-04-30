6791b36a28e822487ca60b382370019d52b7f36d
Ravenclaw934 <102623088+Ravenclaw934@users.noreply.github.com>
2022-04-30 15:28:03 -0400

    Update InvalidUserException.java

--- InvalidUserException.java
+++ InvalidUserException.java
@@ -1,4 +1,13 @@
-
+/**
+ * Invalid User Exception
+ *
+ * An exception to be thrown if the User is invalid
+ *
+ * @author Anthony Whittle, L02
+ *
+ * @version 04/30/2022
+ *
+ */
 public class InvalidUserException extends Exception 
 {
 
