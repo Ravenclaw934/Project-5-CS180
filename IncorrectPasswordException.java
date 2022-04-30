be5da0f5313dfdc5b0eb37577373b53dcb92786f
Ravenclaw934 <102623088+Ravenclaw934@users.noreply.github.com>
2022-04-30 15:27:36 -0400

    Update IncorrectPasswordException.java

--- IncorrectPasswordException.java
+++ IncorrectPasswordException.java
@@ -1,4 +1,13 @@
-
+/**
+ * Incorrect Password Exception
+ *
+ * An exception thrown when the password is wrong
+ *
+ * @author Anthony Whittle, L02
+ *
+ * @version 04/30/2022
+ *
+ */
 public class IncorrectPasswordException extends Exception
 {
 
